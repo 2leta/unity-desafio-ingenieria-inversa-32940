fbbdad132870824697ea39fea481017de4797dd3
Julian Galeano <galeanojulian@hotmail.com>
2022-07-25 19:28:27 -0300

    doble shooting,triple and holding

--- DESAFIO - INGENIERIA INVERSA/Assets/Scripts/Cannon.cs
+++ DESAFIO - INGENIERIA INVERSA/Assets/Scripts/Cannon.cs
@@ -14,9 +14,11 @@ public class Cannon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
-            Shoot();
+        if(Input.GetKeyDown(KeyCode.Space)){ Shoot();
         }
+        if(Input.GetKeyDown(KeyCode.J)){doubleshoot(); }
+    if(Input.GetKeyDown(KeyCode.K)){tripleshoot(); }
+    if(Input.GetKey(KeyCode.L)){HoldShoot(); } 
     }
 
 
@@ -29,9 +31,35 @@ public class Cannon : MonoBehaviour
             Invoke("ResetShoot", 1f);
         }
     }
-
+private void doubleshoot()
+{ if(canShoot)
+{ canShoot=false;
+Invoke("bulletinvoker",0.3f);
+Invoke("bulletinvoker",0.8f);
+Invoke ("ResetShoot",1f);
+}}
     private void ResetShoot()
     {
         canShoot = true;
+    }
+    private void bulletinvoker(){
+        Instantiate(munition, transform);
+    }
+    private void tripleshoot(){
+        if(canShoot){
+            canShoot=false;
+            Invoke ("bulletinvoker",0.3f);
+            Invoke ("bulletinvoker",0.8f);
+            Invoke ("bulletinvoker",1.1f);
+            Invoke ("ResetShoot",1.5f);
+        }}
+        private void HoldShoot()
+    {
+        if (canShoot)
+        {
+            canShoot = false;
+            Instantiate(munition, transform);
+            Invoke("ResetShoot", 0.3f);
+        }
     }
 }
